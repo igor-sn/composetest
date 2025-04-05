13666c0abe5a54d75e1045d7da61d1d222f95fb3
Igor Nogueira <igor.nogueira.dev@gmail.com>
2025-04-05 17:40:05 -0300

    refactor: change message

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return f'Hello World! I have been seen {count} times.!'
+    return f'Hello World! I have been seen {count} times.!\n'
